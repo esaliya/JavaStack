23a03f7bf5b44831c9f8770a1e618eec83a1a757
Saliya Ekanayake <esaliya@gmail.com>
2016-05-26 15:10:11 -0400

    adding mmapipc

--- mmapipc/src/main/java/org/saliya/memorymapped/ipc/InterProcComm.java
+++ mmapipc/src/main/java/org/saliya/memorymapped/ipc/InterProcComm.java
@@ -9,6 +9,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class InterProcComm {
     static Bytes readBytes;
@@ -59,10 +60,19 @@ public class InterProcComm {
         readByteBuffer.putInt(Integer.BYTES, 53);
 
         ByteBuffer recv = MPI.newByteBuffer(size*2*Integer.BYTES);
+        int [] recvCounts = new int[size];
+        int [] displas = new int[size];
+        for (int i = 0; i < size; ++i){
+            recvCounts[i] = 2*Integer.BYTES;
+        }
+        System.arraycopy(recvCounts, 0, displas, 1, size - 1);
+        Arrays.parallelPrefix(displas, (m, n) -> m + n);
+
+//        comm.allGatherv(readByteBuffer, 2*Integer.BYTES, MPI.BYTE, recv, recvCounts, displas, MPI.BYTE);
         comm.allGather(readByteBuffer, 2*Integer.BYTES, MPI.BYTE, recv, 2*Integer.BYTES, MPI.BYTE);
         if (rank == 13){
             for (int i = 0; i < size; ++i){
-                System.out.println("-- " + readByteBuffer.getInt(2*i*Integer.BYTES) + " " +readByteBuffer.getInt((2*i+1)*Integer.BYTES));
+                System.out.println("-- " + recv.getInt(2*i*Integer.BYTES) + " " +recv.getInt((2*i+1)*Integer.BYTES));
             }
         }
         comm.barrier();
