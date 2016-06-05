a99d1694c1bef64512e9fc73b721e4b7e30207e8
Saliya Ekanayake <esaliya@gmail.com>
2016-06-05 16:47:49 -0400

    Changing allgatherv

--- OmpiBenchmarks/src/main/java/org/saliya/ompibenchmarks/Program.java
+++ OmpiBenchmarks/src/main/java/org/saliya/ompibenchmarks/Program.java
@@ -10,30 +10,54 @@ import java.util.stream.IntStream;
 
 public class Program {
     static int worldProcRank;
-    static int worldProcCount;
-    static Intracomm worldProcComm;
+    static int worldProcsCount;
+    static Intracomm worldProcsComm;
     static int pointCount;
+    
+    static int[] lengths;
+    static int[] displas;
 
     public static void main(String[] args) throws MPIException {
         pointCount = Integer.parseInt(args[0]);
         int dimension = Integer.parseInt(args[1]);
         int iter = Integer.parseInt(args[2]);
         MPI.Init(args);
-        worldProcComm = MPI.COMM_WORLD;
-        worldProcRank = worldProcComm.getRank();
-        worldProcCount = worldProcComm.getSize();
-        int q = pointCount / worldProcCount;
-        int r = pointCount % worldProcCount;
+        worldProcsComm = MPI.COMM_WORLD;
+        worldProcRank = worldProcsComm.getRank();
+        worldProcsCount = worldProcsComm.getSize();
+        int q = pointCount / worldProcsCount;
+        int r = pointCount % worldProcsCount;
         int myPointCount = r > 0 && worldProcRank < r ? q + 1 : q;
         long time = 0;
         Stopwatch timer = Stopwatch.createUnstarted();
         DoubleBuffer partialBuffer = MPI.newDoubleBuffer(myPointCount*dimension);
         DoubleBuffer fullBuffer = MPI.newDoubleBuffer(pointCount*dimension);
+        DoubleBuffer timeBuffer = MPI.newDoubleBuffer(1);
+        
+        /* Compute lengths and displacements */
+        lengths = getLengthsArray(worldProcsCount, dimension);
+        displas = new int[worldProcsCount];
+        displas[0] = 0;
+        System.arraycopy(lengths, 0, displas, 1, worldProcsCount - 1);
+        Arrays.parallelPrefix(displas, (m, n) -> m + n);
+        
+        /* Generate points - these are not random numbers */
+        generatePoints(myPointCount, dimension, partialBuffer);
+
         for (int i = 0; i < iter; ++i) {
-            generateRandomPoints(myPointCount, dimension, partialBuffer);
             time += allGather(partialBuffer, fullBuffer, dimension, timer);
         }
-        printMessage("Allgatherv time: " + time  + " ms");
+
+        timeBuffer.put(0, time);
+        worldProcsComm.reduce(timeBuffer,1,MPI.DOUBLE,MPI.MIN,0);
+        double minTime = timeBuffer.get(0);
+        timeBuffer.put(0, time);
+        worldProcsComm.reduce(timeBuffer,1,MPI.DOUBLE,MPI.MAX,0);
+        double maxTime = timeBuffer.get(0);
+        timeBuffer.put(0, time);
+        worldProcsComm.reduce(timeBuffer,1,MPI.DOUBLE,MPI.SUM,0);
+        double avgTime = timeBuffer.get(0) / worldProcsCount;
+        printMessage("Allgatherv time in R0 and min max avg in ms: " + time + "\t" + minTime + "\t" + maxTime + "\t" + avgTime);
         MPI.Finalize();
     }
 
@@ -44,31 +68,10 @@ public class Program {
 
     public static long allGather(
         DoubleBuffer partialPointBuffer, DoubleBuffer fullBuffer, int dimension, Stopwatch timer) throws MPIException {
-        int [] lengths = getLengthsArray(worldProcCount, dimension);
-        int [] displas = new int[worldProcCount];
-        displas[0] = 0;
-        System.arraycopy(lengths, 0, displas, 1, worldProcCount - 1);
-        Arrays.parallelPrefix(displas, (m, n) -> m + n);
 
-        fullBuffer.position(displas[worldProcRank]);
-        partialPointBuffer.position(0);
-        fullBuffer.put(partialPointBuffer);
-
-        /* Let's add a busy wait loop to keep things running */
-        int i = 0;
-        long x = 0;
-        while (i < Integer.MAX_VALUE){
-            x += (long)Math.sqrt(20);
-            ++i;
-        }
-
-
+        worldProcsComm.barrier();
         timer.start();
-        // with separate send and recv buffers
-//        worldProcComm.allGatherv(partialPointBuffer, lengths[worldProcRank], MPI.DOUBLE, fullBuffer, lengths, displas, MPI.DOUBLE);
-
-        // with inplace
-        worldProcComm.allGatherv(fullBuffer, lengths, displas, MPI.DOUBLE);
+        worldProcsComm.allGatherv(partialPointBuffer, lengths[worldProcRank], MPI.DOUBLE, fullBuffer, lengths, displas, MPI.DOUBLE);
         timer.stop();
         long elapsedMills = timer.elapsed(TimeUnit.MILLISECONDS);
         timer.reset();
@@ -83,10 +86,12 @@ public class Program {
         return lengths;
     }
 
-    private static void generateRandomPoints(
+    private static void generatePoints(
         int myPointCount, int dimension, DoubleBuffer partialBuffer) {
         partialBuffer.position(0);
         IntStream.range(0,myPointCount).forEach(i -> IntStream.range(0,dimension).forEach(j-> partialBuffer.put(
-            Math.random())));
+                (((i + j) & 1) == 0 ?
+                        (0.9999995 / 1.0000023) :
+                        (1.0000023 / 0.9999995)))));
     }
 }
