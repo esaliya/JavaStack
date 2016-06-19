779745c012d7741cdb9e99d3f93205c46755ae52
Saliya Ekanayake <esaliya@gmail.com>
2016-06-19 11:50:26 -0400

    Adding debugs
    Note. this code is the correct code

--- kmeans/src/main/java/org/saliya/ompi/kmeans/Program.java
+++ kmeans/src/main/java/org/saliya/ompi/kmeans/Program.java
@@ -140,7 +140,7 @@ public class Program {
                         for (int i = 0; i < threadLocalMyNumVec; ++i) {
                             double[] point = points[i + threadLocalVecStartIdx];
                             int dMinIdx = findCenterWithMinDistance(point, immutableCenters);
-                            if (finalItrCount ==0){
+                            if (finalItrCount ==1){
                                 System.out.println("point " + i  + " closest center "  +dMinIdx);
                             }
                             ++pointsPerCenterForThread[threadIndex][dMinIdx];
